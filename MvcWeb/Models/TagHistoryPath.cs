ccb68efb74b5ca897a823710bf8b5621a9535ee3
Jacquelyn525 <JacquelynLittle1@outlook.com>
2023-12-13 20:10:35 -0600

    added appsettings entries and removed defunct models from web project

--- MvcWeb/Models/TagHistoryPath.cs
+++
@@ -1,8 +0,0 @@
-namespace MvcWeb.Models {
-  public class TagHistoryDB {
-    public string DatePath { get; set; } = string.Empty;
-    public string TimePath { get; set; } = string.Empty;
-    public string FileNamePath { get; set; } = string.Empty;
-    public List<TagIdEntry> TagIdEntries { get; set; } = new List<TagIdEntry>();
-  }
-}
